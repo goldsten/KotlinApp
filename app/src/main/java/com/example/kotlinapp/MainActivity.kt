aa19b962a54f41edaadad0ece3ed46479faa2d04
golds <goldsten23@gmail.com>
2022-05-03 15:25:27 +0300

    создаем callback для активити

--- app/src/main/java/com/example/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinapp/MainActivity.kt
@@ -1,38 +1,37 @@
 package com.example.kotlinapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.kotlinapp.databinding.ActivityMainBinding
 import kotlin.math.pow
 import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity() {
 	private lateinit var binding: ActivityMainBinding
+	private var launcher: ActivityResultLauncher<Intent>? = null
 
 	override fun onCreate(s: Bundle?) {
 		binding = ActivityMainBinding.inflate(layoutInflater)
 		super.onCreate(s)
 		setContentView(binding.root)
 
+		//создаем callback, регистрируемся на результат с аквтивити
+		launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+			// переменная которая создержит возвращаемую информацию
+				result: ActivityResult ->
+				if (result.resultCode == RESULT_OK){
+					// intent
+					// если бы мы передавали текст val text = result.data?.getStringExtra("key") - (что бы указать что result.data будет не пустым, нужно дописать result.data? )
+					result.data
+				}
+		}
 
-		binding.btnResult.setOnClickListener {
-			if (!isFieldEmpty()) binding.tvResult.text = getResult()
-		}
+
+
 	}
 
-	private fun isFieldEmpty(): Boolean{
-		binding.apply {
-			if (edA.text.isNullOrEmpty() || edB.text.isNullOrEmpty()) edA.error ="Field value"
-			return edA.text.isNullOrEmpty() || edB.text.isNullOrEmpty()
-		}
-	}
-	private fun getResult():String{
-		val a: Double
-		val b: Double
-		binding.apply {
-			a = edA.text.toString().toDouble()
-			b = edB.text.toString().toDouble()
-		}
-		return sqrt((a.pow(2) + b.pow(2))).toString()
-	}
 }
