ae2b158c86445e6dab9804439fcec551d17cebf0
golds <goldsten23@gmail.com>
2022-05-06 21:13:33 +0300

    -Toolbar: note: r.themes note: получаем toolbar note: изменяем title note: слушатель нажатий note: создание папки меню • если не указать показывать меню как иконки, по умолчанию будет список note: иконка вместо списка note: показываем меню в активити

--- app/src/main/java/com/example/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.kotlinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import com.example.kotlinapp.databinding.ActivityMainBinding
@@ -14,18 +15,30 @@ class MainActivity : AppCompatActivity() {
 		super.onCreate(s)
 		setContentView(binding.root)
 
-
 		// получаем toolbar
 		supportActionBar?.setDisplayHomeAsUpEnabled(true)
 		// изменяем title
 		supportActionBar?.title = "My Action Bar"
+	}
+	//функция выводит меню на экран
+	override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+		//создаем разметку меню
+		// (выбираем макет меню, в какое меню хотим поместить наш макет)
+		menuInflater.inflate(R.menu.menu_toolbar, menu)
+		return true
 	}
 	// прослушивает нажатие на какой то элемент в toolbar
 	override fun onOptionsItemSelected(item: MenuItem): Boolean {
 		// слушатель нажатий
 		// проверяем или нажали то что нужно
 		// ndroid.R.id.home - так как это встроенный toolbar и его элемент, берем его из android
-		if(item.itemId == android.R.id.home) Toast.makeText(this, "Tap back", Toast.LENGTH_SHORT).show()
+		when(item.itemId){
+			// стрелка назад это дефолтный ид, по этому android.R.id.
+			android.R.id.home -> Toast.makeText(this, "Tap back", Toast.LENGTH_SHORT).show()
+			// это созданый ид, по этому без android
+			R.id.newTitle -> Toast.makeText(this, "Tap menu", Toast.LENGTH_SHORT).show()
+		}
+
 		return true
 	}
 }
