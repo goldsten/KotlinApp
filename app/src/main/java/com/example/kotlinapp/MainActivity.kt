21cd9589539443bcf93652065074f1385385edc7
golds <goldsten23@gmail.com>
2022-05-06 11:54:30 +0300

    -Toolbar: note r.themes note получаем toolbar note изменяем title note слушатель нажатий

--- app/src/main/java/com/example/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.kotlinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import com.example.kotlinapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +15,17 @@ class MainActivity : AppCompatActivity() {
 		setContentView(binding.root)
 
 
+		// получаем toolbar
+		supportActionBar?.setDisplayHomeAsUpEnabled(true)
+		// изменяем title
+		supportActionBar?.title = "My Action Bar"
+	}
+	// прослушивает нажатие на какой то элемент в toolbar
+	override fun onOptionsItemSelected(item: MenuItem): Boolean {
+		// слушатель нажатий
+		// проверяем или нажали то что нужно
+		// ndroid.R.id.home - так как это встроенный toolbar и его элемент, берем его из android
+		if(item.itemId == android.R.id.home) Toast.makeText(this, "Tap back", Toast.LENGTH_SHORT).show()
+		return true
 	}
 }
