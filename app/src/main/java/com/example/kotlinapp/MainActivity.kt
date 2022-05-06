750ded3f35247af5af136840c15e6f400d738832
golds <goldsten23@gmail.com>
2022-05-07 02:03:44 +0300

    -BottomNavMenu: note: исправляем появление Navbar в Design note: разделяем цвет нажатой кнопки и тулбара note: добавление в drawable селектора цвета в navbar note: добавляем слушатель к item note: действие при нажатии на item note: выбор стартового item

--- app/src/main/java/com/example/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinapp/MainActivity.kt
@@ -15,6 +15,22 @@ class MainActivity : AppCompatActivity() {
 		super.onCreate(s)
 		setContentView(binding.root)
 
+		// что бы выбрать стартовый item
+		binding.bottomNavBar.selectedItemId = R.id.menu_add
 
+		// создаем слушатель
+		binding.bottomNavBar.setOnItemSelectedListener {
+			// проверяем id нажатого элемента
+			when(it.itemId){
+				R.id.menu_home -> binding.txtView.text = "Home Activity"
+				R.id.menu_search -> binding.txtView.text = "Search Activity"
+				R.id.menu_add -> binding.txtView.text = "Add Activity"
+				R.id.menu_liked -> binding.txtView.text = "Liked Activity"
+				R.id.menu_profile -> binding.txtView.text = "Profile Activity"
+			}
+
+			// функция должна возвращать boolean
+			true
+		}
 	}
 }
