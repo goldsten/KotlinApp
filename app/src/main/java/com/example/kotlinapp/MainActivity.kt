aab12ab2a7adc7f9282fd10e2d8a448eb3a7de39
golds <goldsten23@gmail.com>
2022-05-07 20:08:07 +0300

    -DrawerLayout 1.1 note: добавление Navigation View в DrawerLayout note: создание разметки item note: добавление menu в navigationView note: слушатель для кнопок item которые в navigationView note: добавление header

--- app/src/main/java/com/example/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.kotlinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.view.GravityCompat
 import com.example.kotlinapp.databinding.ActivityMainBinding
 
@@ -9,12 +10,28 @@ class MainActivity : AppCompatActivity() {
 	private lateinit var binding: ActivityMainBinding
 
 	override fun onCreate(s: Bundle?) {
-		binding = ActivityMainBinding.inflate(layoutInflater)
 		super.onCreate(s)
+		binding = ActivityMainBinding.inflate(layoutInflater)
 		setContentView(binding.root)
 
 		// открытие sidebar
 		binding.apply {
+			// слушатель кнопок в navigation menu
+			navViewId.setNavigationItemSelectedListener {
+				//it это item а itemID id элемента
+				when(it.itemId){
+					// this@MainActivity - указываем конкретно this (эта функция) и куда передаем MainActivity
+					R.id.msg -> Toast.makeText(this@MainActivity, "Message", Toast.LENGTH_SHORT).show()
+					R.id.multimedia -> {
+						Toast.makeText(this@MainActivity, "Multimedia", Toast.LENGTH_SHORT).show()
+					}
+					R.id.setting -> {Toast.makeText(this@MainActivity, "Setting", Toast.LENGTH_SHORT).show()}
+				}
+				// что бы после нажатия закрывать drawer layout
+				drawerL.closeDrawer(GravityCompat.START)
+
+				true
+			}
 			open.setOnClickListener {
 				// выбираем кнопку, по нажатию которой будет определять GravityCompat
 				drawerL.openDrawer(GravityCompat.START)
