ec04077e589eb7777d54e9fb18952af6f9bbc23e
golds <goldsten23@gmail.com>
2022-05-07 03:04:53 +0300

    -DrawerLayout - контейнер в который можно добавить элементы которые будут выдвигаться слева на/с право на лево note: DrawerLayout пишется в коде note: что бы элемент двигался, нужно 1. основной контейнер в котором будет находится элементы 2. контейнер с элементами, который выдвигается сбоку с аттрибутом android:layout_gravity=start/end note: кнопка по нажатию которой можно открыть или закрыть gravity

--- app/src/main/java/com/example/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.kotlinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.GravityCompat
 import com.example.kotlinapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,14 @@ class MainActivity : AppCompatActivity() {
 		super.onCreate(s)
 		setContentView(binding.root)
 
+		// открытие sidebar
+		binding.apply {
+			open.setOnClickListener {
+				// выбираем кнопку, по нажатию которой будет определять GravityCompat
+				drawerL.openDrawer(GravityCompat.START)
+				// что бы закрыть gravity
+				// drawerL.closeDrawer(GravityCompat.START)
+			}
+		}
 	}
 }
